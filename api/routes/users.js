166e24ae83b5bc0c13d397559031645475a8af69
Lebogang Diale <lebogang.diale@dadevs.co.za>
2022-02-02 12:38:12 +0200

    commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -18,7 +18,7 @@ router.post('/register', (req, res, next) => {
             let emailExists = data.data[0].fn_email_address_exists;
             
             if(emailExists){
-                res.status(402).json({
+                res.status(401).json({
                     message: 'email exists',
                     emailExists: data
                 });
@@ -78,33 +78,28 @@ router.post('/register', (req, res, next) => {
 //login
 router.post('/login', (req, res, next) => {
     return new Promise((resolve, reject) => {
-        let placeholder = '';
-        let count = 1;
-        const params = Object.keys(req.body).map(key => [(key), req.body[key]]);
+        let user = "'"+req.body.email_address+"'";
+        let user_pass = "'"+req.body.password+"'";
 
-        const paramsvalues = Object.keys(req.body).map(key => req.body[key]);
+        const loginFn = `moviespot_schema.fn_user_logintest(${user},${user_pass})`;
 
-        if (Array.isArray(params)) {
-            params.forEach(() => {
-                placeholder += `$${count},`;
-                count += 1;
-            });
-        } 
-
-        placeholder = placeholder.replace(/,\s*$/, ''); 
-
-        const functionName = `moviespot_schema.fn_user_logintest`;
-
-        const sql = `${functionName}(${placeholder})`;
-
-        postgres.callFnWithResultsAdd(functionName, [user, user_pass])
+        postgres.callFnWithResultsAdd(loginFn)
         .then((data) => {
             console.log(data);
-   
-            res.status(201).json({
-                message: 'Newly Added user',
-                addedUser: data
-            });
+            let loggedIn = data.data[0].fn_user_logintest;
+
+            if(loggeIn){
+                res.status(201).json({
+                    message: 'User logged in',
+                    addedUser: data
+                });
+            }else{
+                res.status(401).json({
+                    message: 'Incorrect login details',
+                    addedUser: data
+                });
+            }
+            
             resolve(data);
 
         })
