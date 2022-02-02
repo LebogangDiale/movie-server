1683d8e223e50deaa35a71b6a413b939b3878215
Lebogang Diale <lebogang.diale@dadevs.co.za>
2022-02-02 10:15:12 +0200

    testing heroku

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 app.use('/users', userRoutes);
 
 app.use((req, res, next) => {
-    const error = new Error('Url is not found');
+    const error = new Error('Url is not found!');
     error.status = 404;
     next(error);
 })
